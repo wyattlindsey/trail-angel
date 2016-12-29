6383f3f3ab676419d60c75e1085d85ea9cecc9eb
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-28 21:30:09 -0800

    'Hello, Worldgit add src/containers/app.js'

--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -1,23 +1,26 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { createStore, applyMiddleWare, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import * as reducers from '../reducers';
+// import * as reducers from '../reducers';
 import TrailAngel from './trail-angel';
 
-const createStoreWithMiddleware = applyMiddleWare(thunk)(createStore);
-const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+// const reducer = combineReducers(reducers);
+// const store = createStoreWithMiddleware(reducer);
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <TrailAngel />
-      </Provider>
+      <View>
+        <Text>
+          Hello, World!!
+        </Text>
+      </View>
     );
   }
 };
