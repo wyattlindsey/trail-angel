4782cdb1424068d71ac0f882b7f72e99ff5aa4cd
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-29 01:24:00 -0800

    set up react-redux connect and implement test reducer, actions, component, etc

---
+++ client/src/actions/action-types.js
@@ -0,0 +1,9 @@
+export const TEST_ACTION = 'TEST_ACTION';
+
+export const CREATE_USER = 'CREATE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+export const LOGIN_USER = 'LOGIN_USER';
+
+export const ADD_FAVORITE = 'ADD_FAVORITE';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+import testReducer from './test-reducer';
 import user from './user';
 import favorite from './favorite';
 
 export {
+  testReducer,
   user,
   favorite
 };
