8308fa50d25649120dcd1be2da7f3ced7c7cae28
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-28 22:04:42 -0800

    add action placeholders

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,1 +1,7 @@
-// export const createUser = (user) =>
+export const CREATE_USER = 'CREATE_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+export const LOGIN_USER = 'LOGIN_USER';
+
+export const ADD_FAVORITE = 'ADD_FAVORITE';
+export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
