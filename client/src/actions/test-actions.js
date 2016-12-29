cf8874e62756ac680198d633104f381716544268
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-29 02:55:39 -0800

    add new action types and state to test setup

--- client/src/actions/test-actions.js
+++ client/src/actions/test-actions.js
@@ -1,9 +1,22 @@
 import * as actionTypes from './action-types';
 
-export function testAction() {
+const update = (custom, actionType) => {
   return {
-    type: actionTypes.TEST_ACTION,
-    foo: null,
-    bar: null
+    type: actionType,
+    custom: custom
+  }
+}
+
+export const testIncrement = (custom) => {
+  return (dispatch, getState) => {
+    // you could do something async here before dispatching
+    dispatch(update(custom, actionTypes.TEST_INCREMENT));
+  };
+};
+
+export const testDecrement = (custom) => {
+  return (dispatch, getState) => {
+    // you could do something async here before dispatching
+    dispatch(update(custom, actionTypes.TEST_DECREMENT));
   };
 }
--- client/src/reducers/test-reducer.js
+++ client/src/reducers/test-reducer.js
@@ -4,17 +4,26 @@ import * as actionTypes from '../actions/action-types';
 
 const initialState = {
   foo: 42,
-  bar: 'hello'
+  bar: 'hello',
+  custom: 'nullllll'
 }
 
 export default function testReducer(state = initialState, action = {}) {
   switch (action.type) {
-    case actionTypes.TEST_ACTION:
+    case actionTypes.TEST_INCREMENT:
       return {
         ...state,
-        foo: 21,
-        bar: 'hi'
+        foo: state.foo * 2,
+        bar: state.bar === 'hi' ? 'hello' : state.bar += 'o',
+        custom: action.custom
       };
+    case actionTypes.TEST_DECREMENT:
+      return {
+        ...state,
+        foo: state.foo / 2,
+        bar: state.bar.length < 6 ? 'hi' : state.bar.substring(0, state.bar.length - 1),
+        custom: action.custom
+      }
     default:
       return state;
   }
