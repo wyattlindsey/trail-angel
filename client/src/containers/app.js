0e2464e92d3068b4bbeaa88d757333070a89f3e5
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-28 22:32:49 -0800

    set up redux logger

--- client/src/containers/app.js
+++ client/src/containers/app.js
@@ -4,11 +4,13 @@ import React, { Component } from 'react';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 
 import * as reducers from '../reducers';
 import TrailAngel from './trail-angel';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
 
