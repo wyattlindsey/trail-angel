e525235ea826b01f4d0e82d5aecc68235d46df4a
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-01-01 19:14:54 -0800

    change GET endpoint for trailfaves to use option object to determine username

--- client/src/api/__tests__/trailangel-api.spec.js
+++ client/src/api/__tests__/trailangel-api.spec.js
@@ -14,7 +14,23 @@ describe('trail angel client-side api', () => {
     trailAngelApi.getFavorites(1);
 
     expect(request.get).toHaveBeenCalled();
+    expect(request.get)
+      .toHaveBeenCalledWith(`${baseUrl}/api/trailfaves/`, { userId: 1 });
+  });
 
-    expect(request.get).toHaveBeenCalledWith(`${baseUrl}/api/trailfaves/1`);
+  it('adds a trail favorite', () => {
+    trailAngelApi.addFavorite(1, { userId: 1 });
+
+    expect(request.update).toHaveBeenCalled();
+    expect(request.update)
+      .toHaveBeenCalledWith(`${baseUrl}/api/trailfaves/1}`, { userId: 1 });
+  });
+
+  it('removes a trail favorite', () => {
+    trailAngelApi.removeFavorite(1, { userId: 1 });
+
+    expect(request.remove).toHaveBeenCalled();
+    expect(request.remove)
+      .toHaveBeenCalledWith(`${baseUrl}/api/trailfaves/1}`, { userId: 1 });
   });
 });
--- client/src/api/trailangel-api.js
+++ client/src/api/trailangel-api.js
@@ -3,6 +3,6 @@ import * as request from '../utils/request';
 
 export const baseUrl = paths.trailAngel.baseUrl;
 
-export const getFavorites = (userId) => {
-  return request.get(`${baseUrl}/api/trailfaves/${userId}`);
+export const getFavorites = (options) => {
+  return request.get(`${baseUrl}/api/trailfaves`, options);
 }
