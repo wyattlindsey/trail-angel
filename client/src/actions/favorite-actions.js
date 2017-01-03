0172a109cc2109fdf6bc1c0785fce9b25de7623c
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-01-03 15:04:09 -0800

    add some favorite actions

--- client/src/actions/favorite-actions.js
+++ client/src/actions/favorite-actions.js
@@ -1,17 +1,54 @@
-import * as actions from './';
+import actionTypes from './action-types';
+import dataApi from '../api';
 
-export function addFavorite() {
+const requestFavorites = (userId) => {
+  return {
+    type: actionTypes.REQUEST_FAVORITES,
+    userId
+  };
+};
+
+const receiveFavorites = (items) => {
+  return {
+    type: actionTypes.RECEIVE_FAVORITES,
+    userId
+  };
+};
+
+export const fetchFavorites = (userId) => {
+  return (dispatch) => {
+    dispatch(requestFavorites(userId));
+
+    // get favorites for user from the server
+    return dataApi.trailAngelApi.getFavorites(userId)
+      .then((data) => {
+        let promises = data.map((item) => {
+          // todo: check to see if the favorite is already in trailsReducer.items
+          // todo: can this just use existing functionality in trail-actions.js?
+          // todo: for now, just pull directly from yelp
+          return dataApi.yelp({ term: item });
+        });
+
+        Promise.all(promises)
+          .then((results) => {
+            return dispatch(receiveFavorites(results));
+          });
+      });
+  };
+};
+
+export const addFavorite = (userId, itemId) => {
   return {
     type: actions.ADD_FAVORITE,
     userId,
     itemId
   };
-}
+};
 
-export function removeFavorite() {
+export const removeFavorite = (userId, itemId) => {
   return {
     type: actions.REMOVE_FAVORITE,
     userId,
     itemId
   };
-}
+};
