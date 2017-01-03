ae316cfd5daac759f835a478038625f1c2451c60
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-01-02 21:19:28 -0800

    adding trailList back into repo

---
+++ client/src/components/trail/trailList.component.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, ListView, StyleSheet, Text } from 'react-native';
+import Row from './trailListItem.component';
+
+const styles = StyleSheet.create({
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
+  },
+});
+
+
+
+export default class TrailList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.state = {
+      trails: [],
+      dataSource: this.ds.cloneWithRows(['hi']),
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchTrailsIfNeeded({})
+      .then((data) => {
+        debugger;
+        this.setState({
+          trails: data.items.businesses,  // not ideal for this component to have to know about
+                                          // yelp `.businesses` field
+          dataSource: this.ds.cloneWithRows(data.items.businesses)
+        });
+      });
+  }
+
+  render() {
+    return (
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={(data) => <Row {...data} />}
+        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+      />
+    );
+  }
+}
