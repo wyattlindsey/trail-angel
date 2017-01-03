6595df2fffe26ce02a7d054252d75c5c68243424
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-01-03 15:52:11 -0800

    implement primary reducer methods

--- client/src/reducers/favorites-reducer.js
+++ client/src/reducers/favorites-reducer.js
@@ -1,9 +1,40 @@
 'use strict';
 
+import actionTypes from '../actions/action-types';
+
 const initialState = {
-  favorites: []
+  isFetching: false,
+  items: []
 }
 
 export default function favoritesReducer(state = initialState, action = {}) {
-  return state;
-}
+  switch(action.type) {
+    case actionTypes.REQUEST_FAVORITES:
+      return {
+        ...state,
+        isFetching: true
+      };
+    case actionTypes.RECEIVE_FAVORITES:
+      return {
+        ...state,
+        isFetching: false,
+        items: action.items
+      };
+    case actionTypes.ADD_FAVORITE:
+      return {
+        ...state,
+        items: state.items.push(action.item)
+      };
+    case actionTypes.REMOVE_FAVORITE:
+      const index = state.items.findIndex(action.item);
+      const arr = state.items.slice();
+      arr.splice(index, 1)
+
+      return {
+        ...state,
+        items: arr
+      };
+    default:
+      return state;
+  }
+};
