b8a9a42b098167118444da4a55ec0b6d11f05711
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-01-03 00:58:25 -0800

    move icons out into modules

---
+++ client/src/components/icons/index.js
@@ -0,0 +1,13 @@
+import homeIcon from './home-icon';
+import searchIcon from './search-icon';
+import favoriteIcon from './favorite-icon';
+import moreIcon from './more-icon';
+
+const icons = {
+  homeIcon,
+  searchIcon,
+  favoriteIcon,
+  moreIcon
+};
+
+export default icons;
