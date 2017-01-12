3e4fc629c01bd06bc57401b9d418fa6a16dcf0d4
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-01-12 11:23:36 -0800

    add weather info

--- client/src/components/trail/map.component.js
+++ client/src/components/trail/map.component.js
@@ -10,17 +10,65 @@ import {
 import MapView from 'react-native-maps';
 import dataApi from '../../api';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import WeatherIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 var { height, width} = Dimensions.get('window');
 
+var conditions = {
+  'partly-cloudy-night': 'weather-partlycloudy',
+  'partly-cloudy-day': 'weather-partlycloudy',
+  'rain': 'weather-rainy',
+  'clear-day': 'weather-sunny',
+  'clear-night': 'weather-sunny',
+  'cloudy': 'weather-cloudy',
+  'snow': 'weather-snowy',
+  'windy': 'weather-windy'
+};
+
+function dayConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var hour = a.getHours() > 12 ? (a.getHours() - 12) : a.getHours();
+  var min = a.getMinutes() < 10 ? ('0' + a.getMinutes()) : a.getMinutes();
+  var ap = a.getHours() < 12 ? 'AM' : 'PM';
+  var time = hour + ':' + min + ' ' + ap;
+  return time;
+}
+
+
+
 const styles = StyleSheet.create({
   rowContainer: {
     marginTop: 60,
-    flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'center'
+  },
+  tempInfo: {
+    flexGrow: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  weather: {
+    textAlign: 'center'
+  },
+  text: {
+    textAlign: 'center',
+    padding: 2
+  },
+  tempTitle: {
+    padding: 10
   },
   map: {
     width: width,
@@ -56,9 +104,9 @@ export default class TrailMap extends React.Component {
   componentWillMount() {
     let that = this;
     dataApi.weather(this.props.location.coordinate.latitude, this.props.location.coordinate.longitude)
-     .then( (res) => {
+      .then( (res) => {
         that.setState({ weather: res });
-     });
+      });
   }
 
   // onRegionChange(region) {
@@ -98,22 +146,31 @@ export default class TrailMap extends React.Component {
             style={styles.pinImage}
             coordinate={marker.coordinate}
             title={marker.title}
-            image={marker.image}
           />
         </MapView.Animated>
-        <View style={styles.rowContainer} >
-          <Text> Current Conditions </Text>
+        <View style={styles.tempInfo} >
+          <Text style={styles.tempTitle}> Current Conditions </Text>
           { !!(this.state.weather.currently) ?
             <View>
-              <Icon name='map' size={50} color='darkgreen' />
-              <Text>
-                Temperature: {this.state.weather.currently.temperature}
+              <Text style={styles.weather}>
+                <WeatherIcon name={conditions[this.state.weather.currently.icon]} size={40} style={{}}/>
+              </Text>
+              <Text style={styles.text}>
+                {this.state.weather.currently.summary}
+              </Text>
+              <Text style={styles.text}>
+                Temperature: {Math.floor(this.state.weather.currently.temperature)} &#x2109;
+              </Text>
+              <Text style={styles.text}>
+                <WeatherIcon name='weather-sunset-up' size={25} /> {timeConverter(this.state.weather.daily.data[0].sunriseTime)}
+              </Text>
+              <Text style={styles.text}>
+                <WeatherIcon name='weather-sunset' size={25} /> {timeConverter(this.state.weather.daily.data[0].sunsetTime)}
               </Text>
             </View>
-          : null}
+            : null}
         </View>
       </View>
     )
   }
 }
-
