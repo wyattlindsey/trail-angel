ec106433594dc8782e034c04026e9ef4f7dda506
Andrew Cookro <andrew.cookro@gmail.com>
2017-01-13 15:14:34 -0800

    Added getLocation method in google-api to retrieve city to populate current location in profile

--- client/src/api/google-api.js
+++ client/src/api/google-api.js
@@ -21,6 +21,31 @@ const googleApi = {
       .catch((err) => {
         console.error('Error fetching distance data: ', err);
       });
+  },
+
+  getCity: (coords) => {
+    // so things don't explode
+    if (coords === undefined) {
+      return Promise.resolve(false);
+    }
+
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.latitude},${coords.longitude}&key=${secrets.googleGeocoding.apiKey}`;
+
+    return request.get(url)
+    .then((response) => {
+      console.log(response.results[0].formatted_address);
+      if (response.results[0].formatted_address !== undefined) {
+          var city = response.results[0].formatted_address.split(', ')[1];
+          console.log(city);
+          return city;
+        } else {
+          return false;
+        }
+      })
+      .catch((err) => {
+        console.error('Error fetching city data: ', err);
+      });
+
   }
 }
 
