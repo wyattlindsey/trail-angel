0cb4f529b88772252080263ed906555b791a6019
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-01-19 10:12:29 -0800

    start up new storage actions file

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,13 +3,15 @@ import * as appActions from './app-actions';
 import listingsActions from './listing-actions';
 import searchActions from './search-actions';
 import favoriteActions from './favorite-actions';
+import storageActions from './storage-actions';
 
 const actions = {
   appActions,
   listingsActions,
   userActions,
   searchActions,
-  favoriteActions
+  favoriteActions,
+  storageActions
 };
 
 export default actions;
---
+++ client/src/actions/storage-actions.js
@@ -0,0 +1,22 @@
+import actionTypes from './action-types';
+import dataApi from '../api';
+
+const storageActions = {
+  loadListingsFromStorage: () => {
+
+  },
+
+  saveToStorage: (listing) => {
+
+  },
+
+  removeFromStorage: (id) => {
+
+  },
+
+  clearAllListingsFromStorage: () => {
+
+  }
+};
+
+export default storageActions;
