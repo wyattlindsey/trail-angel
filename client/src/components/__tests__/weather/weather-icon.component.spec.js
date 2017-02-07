09ed5ca87c24b23d1354ad88f628cbd819ad845b
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-02-06 22:56:36 -0800

    add unit test for weather icons

---
+++ client/src/components/__tests__/weather/weather-icon.component.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import { View } from 'react-native';
+import React from 'react';
+
+import WeatherIcon from '../../weather/weather-icon.component';
+import weatherData from '../../../../__tests__/fixtures/weather-data';
+
+describe('weather icon component', () => {
+  it('renders correctly', () => {
+    console.log(weatherData.daily);
+    const tree = (
+      <View>
+        <WeatherIcon icon={weatherData.daily.data[0].icon} />
+        <WeatherIcon icon={weatherData.daily.data[1].icon} />
+        <WeatherIcon icon={weatherData.daily.data[2].icon} />
+      </View>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
