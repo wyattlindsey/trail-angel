d1801dec7deae100a4abb78b933bfeed5aa86f4e
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-02-06 23:11:24 -0800

    clean up commented out response handling for update method

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -45,10 +45,6 @@ const request = {
       body: JSON.stringify(options),
       headers
     })
-      // .then((response) => response.json())
-      // .then((responseJSON) => {
-      //   return responseJSON;
-      // })
       .catch((err) => {
         console.error('error updating data on server', err);
       });
