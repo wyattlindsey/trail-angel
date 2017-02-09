fcaf4d8d8899ed9550d279b9b3d3cfa3d98de575
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-02-08 17:31:40 -0800

    finish adding http methods to request.js unit tests

--- client/src/utils/__tests__/request.spec.js
+++ client/src/utils/__tests__/request.spec.js
@@ -10,20 +10,58 @@ describe('http request module', () => {
     fetchMock.restore();
   });
 
-  const url = 'https://api.darksky.net/forecast/123/456/789';
+  const weatherUrl = 'https://api.darksky.net/forecast/123/456/789';
+  const apiUrl = 'https://www.trailangel.com/api';
+  const id = 1234
 
   it('should handle get requests', () => {
-    fetchMock.mock(url, weatherData, { times: 1 });
-    return request.get(url)
+    fetchMock.get(weatherUrl, weatherData, { times: 1 });
+    return request.get(weatherUrl)
       .then((response) => {
-        expect(fetchMock.called(url)).toBe(true);
-        expect(fetchMock.done(url)).toEqual(true);
-        expect(fetchMock.lastCall(url)).toEqual([url, {}]);
+        expect(fetchMock.done(weatherUrl)).toBe(true);
+        expect(fetchMock.lastCall(weatherUrl)).toEqual([weatherUrl, {}]);
         expect(response).toEqual(weatherData);
       });
   });
 
-  // it('should pass options correctly', () => {
-  //   fetchMock.mock()
-  // });
+  it('should pass options correctly', () => {
+    fetchMock.get(weatherUrl, weatherData);
+    request.get(weatherUrl, { foo: 'bar' });
+    expect(fetchMock.lastCall(weatherUrl)).toEqual([weatherUrl, { foo: 'bar' }]);
+  });
+
+  it('should call endpoint with id when using `show()`', () => {
+    const url = `${apiUrl}/${id}`;
+    fetchMock.get(url, { foo: 'bar' }, { times: 1 });
+    return request.show(apiUrl, id)
+      .then((response) => {
+        expect(fetchMock.done(url)).toBe(true);
+        expect(fetchMock.lastUrl(url));
+        expect(response).toEqual({ foo: 'bar' });
+      });
+  });
+
+  it('should handle post requests', () => {
+    fetchMock.post(apiUrl, 201, { times: 1 });
+    request.add(apiUrl, { foo: 'bar' });
+    expect(fetchMock.done(apiUrl)).toBe(true);
+    expect(fetchMock.lastUrl(apiUrl)).toBe(apiUrl);
+    expect(fetchMock.lastOptions(apiUrl)).toEqual({"body": "{\"foo\":\"bar\"}", "headers": {"Accept": "application/json", "Content-Type": "application/json"}, "method": "POST"});
+  });
+
+  it('should handle put requests', () => {
+    fetchMock.put(apiUrl, 204, { times: 1 });
+    request.update(apiUrl, { foo: 'bar' });
+    expect(fetchMock.done(apiUrl)).toBe(true);
+    expect(fetchMock.lastUrl(apiUrl)).toBe(apiUrl);
+    expect(fetchMock.lastOptions(apiUrl)).toEqual({"body": "{\"foo\":\"bar\"}", "headers": {"Accept": "application/json", "Content-Type": "application/json"}, "method": "PUT"});
+  });
+
+  it('should handle delete requests', () => {
+    fetchMock.delete(apiUrl, 200, { times: 1 });
+    request.remove(apiUrl, { foo: 'bar' });
+    expect(fetchMock.done(apiUrl)).toBe(true);
+    expect(fetchMock.lastUrl(apiUrl)).toBe(apiUrl);
+    expect(fetchMock.lastOptions(apiUrl)).toEqual({"body": "{\"foo\":\"bar\"}", "headers": {"Accept": "application/json", "Content-Type": "application/json"}, "method": "DELETE"});
+  });
 });
