512806c032ecb070146056ec1634b6cf4f8b5b49
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-02-14 10:59:41 -0800

    swap out urls for heroku env variables

--- server/config/environment/staging.js
+++ server/config/environment/staging.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  MYSQL_DATABASE_URL: 'mysql://eayjbr8g5akqk19u:va09dmifobpgce3d@l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cqlv5eav3fvny29o',
+  MYSQL_DATABASE_URL:     process.env.JAWSDB_URL,
 
-  REDIS_URL:         'redis://h:pf7d97a0a9388ff07f46d8006357cbe7befc7f3c375cdcc15a68eea468522e1df@ec2-34-198-54-21.compute-1.amazonaws.com:11529'
+  REDIS_URL:              process.env.REDIS_URL
 };
