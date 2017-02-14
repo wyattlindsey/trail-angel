744b9919b7be9a66fc2080b7d8c55ca1b44ad476
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2017-02-14 09:52:59 -0800

    add real redis url to staging config

--- server/config/environment/staging.js
+++ server/config/environment/staging.js
@@ -3,5 +3,5 @@
 module.exports = {
   MYSQL_DATABASE_URL: 'mysql://eayjbr8g5akqk19u:va09dmifobpgce3d@l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cqlv5eav3fvny29o',
 
-  REDIS_HOST:         'localhost'
+  REDIS_HOST:         'redis://h:pf7d97a0a9388ff07f46d8006357cbe7befc7f3c375cdcc15a68eea468522e1df@ec2-34-198-54-21.compute-1.amazonaws.com:11529'
 };
