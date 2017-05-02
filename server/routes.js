9ff051053610a8e99423499c5f759e4bfab2e59b
Andrew Cookro <andrew.cookro@gmail.com>
2017-05-02 01:18:28 -0400

    Add supplyitem route

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,7 @@ module.exports = function(app) {
   app.use('/api/users', require('./api/route/user.route.js'));
   app.use('/api/trailfaves', require('./api/route/trailfaves.route.js'));
   app.use('/api/geolocations', require('./api/route/geolocation.route.js'));
+  app.use('/api/supplyitems', require('./api/route/supplyitems.route.js'));
 
   app.route('/*')
     .get(function(req, res) {
